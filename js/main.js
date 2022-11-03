a1d823a3c42ca2ea914f624a8da80019c7b60d3d
DiogoMendes <66794485+DevDeezy@users.noreply.github.com>
2022-11-03 11:33:28 +0000

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -100,3 +100,12 @@ $(document).ready(function() {
     };
 
 })(jQuery);
+
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
